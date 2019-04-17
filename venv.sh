1402ae21f1266fa4a1a200176593c39b17a01743
Sergio Patino <sergio@vertigobox.org>
2019-04-17 11:28:07 -0700

    Pin YAML version

--- venv.sh
+++ venv.sh
@@ -25,6 +25,6 @@ fi
 
 # Install or upgrade dependencies
 echo ':: PYTHON 2'
-"$venv/neovim2/bin/pip" install -U pynvim PyYAML
+"$venv/neovim2/bin/pip" install -U pynvim PyYAML==3.13
 echo -e '\n:: PYTHON 3'
-"$venv/neovim3/bin/pip" install -U pynvim PyYAML Send2Trash
+"$venv/neovim3/bin/pip" install -U pynvim PyYAML==3.13 Send2Trash
